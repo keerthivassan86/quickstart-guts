97e6f4c43cad50ce15f5a3bfc01569c5a27f2c71
Alok Kumar <rajalokan@gmail.com>
2016-08-23 11:25:44 +0530

    added dashboard setup

---
+++ dashboard.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [[ "$#" -ne 1 ]]; then
+    echo "Keystone host not passed. Please try again with keystone host as argument "
+    exit 0
+fi
+
+KEYSTONE_HOST=$1
+
+sudo apt-get install -y openstack-dashboard
+
+# Change local_settings to use version 3.
+sudo bash -c 'cat << EOF >> /etc/openstack-dashboard/local_settings.py
+
+OPENSTACK_API_VERSIONS = {
+    "identity": 3
+}
+EOF'
+
+sudo sed -i "s@^OPENSTACK_HOST.*@OPENSTACK_HOST = '${KEYSTONE_HOST}'@" local_settings.py
+
+sudo service apache2 restart
+
+sudo apt-add-repository 'deb [arch=amd64] http://guts.stackbuffet.com/deb/ trusty-updates/mitaka main'
+
+sudo bash -c 'cat << EOF > /etc/apt/preferences
+Package: *
+Pin: origin "180.148.27.143"
+Pin-Priority: 999
+EOF'
+
+sudo bash -c 'cat << EOF > /etc/apt/apt.conf.d/98stackbuffet
+APT::Get::AllowUnauthenticated "true";
+EOF'
+
+sudo apt-get update
+
+sudo apt-get install -y guts-dashboard
