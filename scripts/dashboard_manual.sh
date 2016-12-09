f1698149f74e5b6b25a6b198b2fca3c32359d63f
Alok Kumar <rajalokan@gmail.com>
2016-12-10 00:06:21 +0530

    simple changes

--- scripts/dashboard_manual.sh
+++ scripts/dashboard_manual.sh
@@ -15,7 +15,7 @@ STACK_DIR="/opt/stack"
 HORIZON_DIR=${STACK_DIR}/horizon
 sudo mkdir -p /opt/stack && sudo chown -R ubuntu:ubuntu ${STACK_DIR}
 git clone https://github.com/openstack/horizon.git -b stable/mitaka ${HORIZON_DIR}
-cd ${HORIZON_DIR} && pip install -e .
+cd ${HORIZON_DIR} && sudo -H pip install -e .
 mv ${HORIZON_DIR}/openstack_dashboard/local/local_settings.py.example ${HORIZON_DIR}/openstack_dashboard/local/local_settings.py
 
 cat >> ${HORIZON_DIR}/openstack_dashboard/local/local_settings.py << EOF
@@ -35,6 +35,9 @@ cd ${GUTS_DASHBOARD_DIR} && pip install -e .
 git clone https://github.com/aptira/python-gutsclient ${STACK_DIR}/python-gutsclient
 pip install -e ${STACK_DIR}/python-gutsclient
 ln -s ${GUTS_DASHBOARD_DIR}/gutsdashboard/local/_50_guts.py ${HORIZON_DIR}/openstack_dashboard/local/enabled/_50_guts.py
+ln -s ${GUTS_DASHBOARD_DIR}/gutsdashboard/local/_5010_guts_services.py ${HORIZON_DIR}/openstack_dashboard/local/enabled/_5010_guts_services.py
+ln -s ${GUTS_DASHBOARD_DIR}/gutsdashboard/local/_5020_guts_resources.py ${HORIZON_DIR}/openstack_dashboard/local/enabled/_5020_guts_resources.py
+ln -s ${GUTS_DASHBOARD_DIR}/gutsdashboard/local/_5030_guts_migrations.py ${HORIZON_DIR}/openstack_dashboard/local/enabled/_5030_guts_migrations.py
 cd ${HORIZON_DIR} && ./manage.py runserver 0.0.0.0:8888
 
 
